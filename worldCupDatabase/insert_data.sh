29913e6cdc729d4c163d4c23650ab0927fb789d5
James Legros <james.legros@mail.mcgill.ca>
2022-09-21 12:06:28 -0400

    Update insert_data.sh

--- worldCupDatabase/insert_data.sh
+++ worldCupDatabase/insert_data.sh
@@ -8,3 +8,39 @@ else
 fi
 
 # Do not change code above this line. Use the PSQL variable above to query your database.
+echo $($PSQL "TRUNCATE teams, games")
+
+# Reset sequences
+echo $($PSQL "SELECT setval('teams_team_id_seq', 1, false)")
+echo $($PSQL "SELECT setval('games_game_id_seq', 1, false)")
+
+cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
+do
+  # Print test
+  # echo "$YEAR | $ROUND | $WINNER | $OPPONENT | $WINNER_GOALS | $OPPONENT_GOALS"
+
+  # Skip header row
+  if [[ $YEAR != "year" ]]
+  then
+
+    # get team_id for Winner
+    WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
+
+    # if not found
+    if [[ -z $WINNER_ID ]]
+    then
+
+      # insert team (name)
+      INSERT_TEAM_RESULT=$($PSQL "INSERT INTO teams(name) VALUES ('$WINNER')")
+
+      # get new team_id
+      WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
+    fi
+
+    # TESTS
+    
+    # Test Winner Inserts
+    echo Winner: $WINNER_ID, $WINNER
+  fi
+    
+done
